521febdd000f0bf1859b042d031f6fa07e8dc793
Saurabh_Bakolia <skbakolia2002@gmail.com>
2021-11-26 12:53:23 +0530

    added "add title"

--- js/app.js
+++ js/app.js
@@ -3,29 +3,10 @@ console.log(`Hey I'm app.js speaking`);
 showNotes();
 
 // if user add a note, add it to the localStorage
-let addTitle = document.getElementById("addTitle");
-addTitle.addEventListener("click", function (e) {
-    let addTxt = document.getElementById("addTxt");
-    let title = localStorage.getItem("title");
-
-    if (title == null) {
-        titleObj = [];
-    } else {
-        titleObj = JSON.parse(title);
-    }
-
-    titleObj.push(addTxt.value);
-    localStorage.setItem("title", JSON.stringify(titleObj));
-    addTxt.value = "";
-    // console.log(notesObj);
-    showNotes();
-
-});
-
-
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
 
     if (notes == null) {
@@ -34,12 +15,17 @@ addBtn.addEventListener("click", function (e) {
         notesObj = JSON.parse(notes);
     }
 
-    notesObj.push(addTxt.value);
+    let myObj = {
+        text: addTxt.value,
+        title: addTitle.value
+    }
+
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
     // console.log(notesObj);
     showNotes();
-
 });
 
 // This function is to show the notes, added by user
@@ -57,25 +43,23 @@ function showNotes() {
 
     notesObj.forEach(function (element, index) {
         html += `
-        <div class="noteCard mx-2 my-2 card" style="width: 18rem;">
-            <div class="card-body">
-            <button type="button" class="btn btn-primary" style="margin-bottom: 10px;" id="addTitle">${title}</button>
-                <p class = "card-text">${element}</p>
-                <button id ="${index}" onclick = "deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-            </div>
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${element.title}</h5>
+            <p class="card-text"> ${element.text}</p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
+    </div>
         `;
     });
 
-    let notesElm = document.getElementById('notes');
+    let notesElm = document.getElementById("notes");
     if (notesObj.length != 0) {
         notesElm.innerHTML = html;
-    }
-    else {
-        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`
+    } else {
+        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
     }
 }
-
 
 // fucntion to delete a note
 function deleteNote(index) {
@@ -89,33 +73,29 @@ function deleteNote(index) {
     }
 
     notesObj.splice(index, 1);
-    localStorage.setItem('notes', JSON.stringify(notesObj));
+    localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
 }
 
-
 // Search Fearture
-let search = document.getElementById('searchTxt');
-search.addEventListener('input', function () {
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
     let inputVal = search.value.toLowerCase();
     // console.log('Input event fired!', inputVal);
-    let noteCards = document.getElementsByClassName('noteCard');
+    let noteCards = document.getElementsByClassName("noteCard");
     Array.from(noteCards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
         // console.log(cardTxt);
 
-
         if (cardTxt.toLowerCase().includes(inputVal)) {
             element.style.display = "block";
-        }
-        else {
+        } else {
             element.style.display = "none";
         }
-    })
-})
+    });
+});
 
-
-/* 
+/*
 Further Fearures:
 1. Add Title
 2. Mark a note as Important
